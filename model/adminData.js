35ab218d5209341180a9aeec62304fd55d89ca24
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2023-03-15 22:56:31 +0530

    remove all old server files

--- model/adminData.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const adminSchema = new Schema({
-    email: String,
-    password: String
-});
-
-module.exports = mongoose.model('admin', adminSchema, 'admin');
