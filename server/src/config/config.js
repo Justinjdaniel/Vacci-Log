455895fc9d641e484759ffd2ed229f9a0647fafe
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2023-03-21 23:57:34 +0530

    config added/ updated

---
+++ server/src/config/config.js
@@ -0,0 +1,40 @@
+import { config } from 'dotenv';
+import { number, object, string } from 'joi';
+import { join } from 'path';
+
+config({ path: join(__dirname, '../../.env') });
+
+const envVarsSchema = object()
+  .keys({
+    NODE_ENV: string().valid('production', 'development', 'test').required(),
+    PORT: number().default(3000),
+    MONGODB_URL: string().required().description('Mongo DB url'),
+    JWT_SECRET: string().required().description('JWT secret key'),
+    ALCHEMY_API_KEY: string().description('alchemy api key'),
+    CONTRACT_ADDRESS: string().description('deployed contract address'),
+    ACCOUNT_ADDRESS: string().description('wallet account address'),
+    PRIVATE_KEY: string().description('wallet account private key'),
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: 'key' } })
+  .validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+export const env = envVars.NODE_ENV;
+export const port = envVars.PORT;
+export const mongoose = {
+  url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
+  options: {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+};
+export const jwt = {
+  secret: envVars.JWT_SECRET,
+};
--- server/src/config/mongoDB.js
+++ server/src/config/mongoDB.js
@@ -1,4 +1,4 @@
-import { createConnection, set } from 'mongoose';
+import { connect, set } from 'mongoose';
 
 const MONGO_URI = process.env.MONGO_URI;
 
@@ -6,7 +6,7 @@ set('strictQuery', false);
 
 const connectDB = async () => {
   try {
-    const db = await createConnection(MONGO_URI);
+    const db = await connect(MONGO_URI);
     console.log(
       `\x1b[4m\u001b[46;1m MongoDB Connected:\u001b[44;1m ${db.name} DB \u001b[0m`
     );
