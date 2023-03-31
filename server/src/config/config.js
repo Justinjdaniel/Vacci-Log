b1032d123fe7e4919029dc40646c74c42e6c0062
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2023-04-01 03:49:27 +0530

    config updated

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,4 +1,5 @@
 import { config } from 'dotenv';
+import 'dotenv/config';
 import joi from 'joi';
 import { join } from 'path';
 import __dirname from '../../dirname.js';
@@ -6,14 +7,18 @@ import __dirname from '../../dirname.js';
 config({ path: join(__dirname, '../../.env') });
 
 const envVarsSchema = joi
-  .object()
-  .keys({
+  .object({
     NODE_ENV: joi
       .string()
       .valid('production', 'development', 'test')
       .required(),
     PORT: joi.number().default(3000),
     MONGODB_URL: joi.string().required().description('Mongo DB url'),
+    MONGODB_USERNAME: joi.string().required().description('Mongo DB user'),
+    MONGODB_PASSWORD: joi
+      .string()
+      .required()
+      .description('Mongo DB user password'),
     JWT_SECRET: joi.string().required().description('JWT secret key'),
     ALCHEMY_API_KEY: joi.string().description('alchemy api key'),
     CONTRACT_ADDRESS: joi.string().description('deployed contract address'),
@@ -22,9 +27,9 @@ const envVarsSchema = joi
   })
   .unknown();
 
-const { value: envVars, error } = envVarsSchema
-  .prefs({ errors: { label: 'key' } })
-  .validate(process.env);
+const { value: envVars, error } = envVarsSchema.validate(process.env, {
+  errors: { label: 'key' },
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -33,7 +38,10 @@ if (error) {
 export const env = envVars.NODE_ENV;
 export const port = envVars.PORT;
 export const mongoose = {
-  url: envVars.MONGODB_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
+  url: `${envVars.MONGODB_URL.replace(
+    '<username>',
+    envVars.MONGODB_USERNAME
+  ).replace('<password>', envVars.MONGODB_PASSWORD)}${envVars.NODE_ENV ?? ''}`,
   options: {
     useCreateIndex: true,
     useNewUrlParser: true,
